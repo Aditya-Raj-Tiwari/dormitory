373a9ec805ca3e2406354693222895efd9b1b6d4
Aditya-Raj-Tiwari <artrajtiwari@gmail.com>
2021-07-20 23:48:10 +0200

    header section initial commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,14 @@
-$(document).ready(function(){
-    $('.owl-carousel-header').owlCarousel();
-  });
+$(document).ready(function () {
+    $('.owl-carousel-header').owlCarousel({
+        navigation: true, // Show next and prev buttons
+        slideSpeed: 300,
+        paginationSpeed: 400,
+        items: 1,
+        itemsDesktop: false,
+        itemsDesktopSmall: false,
+        itemsTablet: false,
+        itemsMobile: false,
+        autoHeight: false,
+        autoHeightClass: 'owl-main-height'
+    });
+});
