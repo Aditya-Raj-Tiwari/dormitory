1f4c46ee1dc84e24c0e0aec3a4a07c931095a9ec
Aditya-Raj-Tiwari <artrajtiwari@gmail.com>
2021-07-21 01:05:43 +0200

    add autoplay and owl dots

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,8 @@ $(document).ready(function () {
     $('.owl-carousel.header_slider').owlCarousel({
         navigation: true,
         navigationText: ["<img src='/resources/left'>", "<img src='mynextimage.png'>"],
+        autoPlay: 3000, 
+        dots: true,
         slideSpeed: 300,
         paginationSpeed: 400,
         items: 1,
