4def92885e470299917170fd4b04ba84a5efa5e9
Aditya-Raj-Tiwari <artrajtiwari@gmail.com>
2021-08-11 21:04:32 +0200

    desktop side complete

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,11 @@
-$(document).ready(function () {
-    $('.owl-carousel.header_slider').owlCarousel({
-        navigation: true,
-        navigationText: ["<img src='/resources/left'>", "<img src='mynextimage.png'>"],
-        autoPlay: 3000, 
-        dots: true,
-        slideSpeed: 300,
-        paginationSpeed: 400,
-        items: 1,
-        autoHeight: false,
-        autoHeightClass: 'owl-main-height'
-    });
+$('#section_1 .owl-carousel').owlCarousel({
+    nav: true,
+    navText: ["<img src='./resources/left.svg' style='width:60px;'>", "<img src='./resources/left.svg' style=' transform: rotate(180deg);width:60px; '>"],
+    autoPlay: 1500,
+    dots: true,
+    slideSpeed: 300,
+    paginationSpeed: 400,
+    items: 1,
+    autoHeight: false
 });
+
